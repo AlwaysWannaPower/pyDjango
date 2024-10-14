537a9903dbeaf1dcc7996a8bab40fec63105631d
Denis Nikulin <augustgants@gmail.com>
2024-10-15 02:03:01 +0300

    09.16 про условные и фильры джи2

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,4 @@
+from timeit import default_timer
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
@@ -5,5 +6,8 @@ from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
+    context = {
+        "time_running": default_timer(),
+    }
 
-    return render(request, "shopapp/shop-index.html")
+    return render(request, "shopapp/shop-index.html", context=context)
